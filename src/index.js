e00c9b2aed4b2234f2839484d4e9a99a1e56d16a
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-08-11 14:34:32 -0400

    fix(index.js): fixed SearchBarProvider import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as SearchBar } from './components/SearchBar'
 export { default as SearchBarMenu } from './components/SearchBarMenu'
-export { SearchBarProvider } from './components/SearchBarProvider'
+export { default as SearchBarProvider } from './components/SearchBarProvider'
 export { SearchBarContext } from './components/SearchBarContext'
