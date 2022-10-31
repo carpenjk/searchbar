29e1fd8f3064e1b0d789cbf2787797e0697b4d78
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-10-31 17:07:18 -0400

    fix(usehasscrollbar): fix ref error

--- src/hooks/useHasScrollbar.js
+++ src/hooks/useHasScrollbar.js
@@ -4,19 +4,19 @@ const useHasVerticalScrollbar = (elem, boundingParent) => {
   const [hasScrollbar, setHasScrollbar] = useState(false)
   function calcHasScrollbar () {
     if (typeof elem.innerWidth === 'number') {
-      if (!elem || !boundingParent) {
-        setHasScrollbar(false)
-        return
-      }
       setHasScrollbar(elem.innerHeight > boundingParent.innerHeight)
     }
   }
 
   useEffect(() => {
+    if (!elem || !boundingParent) {
+      setHasScrollbar(false)
+      return
+    }
     boundingParent.addEventListener('resize', calcHasScrollbar)
     calcHasScrollbar()
     return boundingParent.removeEventListener('resize', calcHasScrollbar)
-  }, [])
+  }, [elem, boundingParent])
 
   return (hasScrollbar)
 }
