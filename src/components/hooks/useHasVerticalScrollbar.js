6463209ab0d260b7246cadbec58d83e52efc225b
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-01-28 18:26:28 -0500

    test

--- src/components/hooks/useHasVerticalScrollbar.js
+++ src/components/hooks/useHasVerticalScrollbar.js
@@ -4,31 +4,30 @@ const useHasVerticalScrollbar = (elem, deps, onChange) => {
   const [hasScrollbar, setHasScrollbar] = useState(false)
   const prevHasScrollbar = useRef(hasScrollbar)
   const _deps = deps ? [...deps] : []
-  const calcHasScrollbar = () => {
-    const onChange2 = onChange
-    console.log('🚀 ~ file: useHasVerticalScrollbar.js:9 ~ calcHasScrollbar ~ onChange2', onChange2)
-    console.log('prevHasScrollbar:', prevHasScrollbar)
-    if (!elem) {
-      setHasScrollbar(false)
-      if (prevHasScrollbar.current !== false && typeof onChange2 === 'function') {
-        console.log('onChange:', false)
-        onChange2(false)
-      }
-      prevHasScrollbar.current = false
-      return
-    }
-    const newHasScrollbar = elem.scrollHeight > elem.clientHeight
-    console.log('calcHasScrollbar:', newHasScrollbar)
-    setHasScrollbar(newHasScrollbar)
-    if (prevHasScrollbar.current !== newHasScrollbar && typeof onChange2 === 'function') {
-      console.log('onChange:', newHasScrollbar)
-      onChange2(newHasScrollbar)
-    }
-    prevHasScrollbar.current = newHasScrollbar
-  }
+
   useEffect(() => {
+    const calcHasScrollbar = () => {
+      console.log('prevHasScrollbar:', prevHasScrollbar)
+      if (!elem) {
+        setHasScrollbar(false)
+        if (prevHasScrollbar.current !== false && typeof onChange2 === 'function') {
+          console.log('onChange:', false)
+          onChange(false)
+        }
+        prevHasScrollbar.current = false
+        return
+      }
+      const newHasScrollbar = elem.scrollHeight > elem.clientHeight
+      console.log('calcHasScrollbar:', newHasScrollbar)
+      setHasScrollbar(newHasScrollbar)
+      if (prevHasScrollbar.current !== newHasScrollbar && typeof onChange2 === 'function') {
+        console.log('onChange:', newHasScrollbar)
+        onChange(newHasScrollbar)
+      }
+      prevHasScrollbar.current = newHasScrollbar
+    }
     window.addEventListener('resize', calcHasScrollbar)
-    calcHasScrollbar()
+    calcHasScrollbar(onChange)
     return () => window.removeEventListener('resize', calcHasScrollbar)
   }, [elem, onChange, ..._deps])
 
