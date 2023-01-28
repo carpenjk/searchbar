934ee718132d78dd6f33750c07dfd5bd516beb8a
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-01-28 14:11:11 -0500

    test

--- src/components/hooks/useHasVerticalScrollbar.js
+++ src/components/hooks/useHasVerticalScrollbar.js
@@ -20,6 +20,7 @@ const useHasVerticalScrollbar = (elem, deps, onChange) => {
       const newHasScrollbar = elem.scrollHeight > elem.clientHeight
       console.log('calcHasScrollbar:', newHasScrollbar)
       setHasScrollbar(newHasScrollbar)
+      debugger
       if (prevHasScrollbar.current !== newHasScrollbar && typeof onChange === 'function') {
         console.log('onChange:', newHasScrollbar)
         onChange(newHasScrollbar)
